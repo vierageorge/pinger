ffa622ccb660c4a1804fbcbc26b7830a93cb1fc7
vierageorge <joviera@gmail.com>
2022-02-05 18:54:55 -0500

    Completing the SNS notification

--- internet_checker/app.py
+++ internet_checker/app.py
@@ -1,6 +1,7 @@
 import logging
 from internet_checker.services.io_service import get_status, set_status
 from internet_checker.services.network_service import get_internet_status
+from internet_checker.services.aws_service import send_notification
 
 logger = logging.getLogger(__name__)
 
@@ -12,6 +13,10 @@ def run():
     if previous_status == current_status:
         logger.info(f"Nothing has changed. Current status: {current_status}")
     else:
-        logger.info("INTERNETS BACK, BABY" if current_status == "OK" else "WE LOST CONNECTION")
+        message = "WE LOST CONNECTION"
+        if current_status == "OK":
+            message = "INTERNETS BACK, BABY"
+            send_notification("Home Notif: INTERNETS BACK, BABY")
+        logger.info(message)
         set_status(current_status)
     logger.info("Process COMPLETED")
